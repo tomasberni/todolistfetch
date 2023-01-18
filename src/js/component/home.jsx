13dd10873c5b4986a478b0603cec6fe8f78a8b08
T0MAS2 <tomasbernimozzo@gmail.com>
2023-01-18 20:36:50 +0000

    todolistflech

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,80 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 const Home = () => {
     const [input, setInput] = useState("");
     const [todos, setTodos] = useState([]);
+    const [todosEnServer, setTodosEnServer] = useState([])
 
 
     function handleInput(e) {
         setInput(e.target.value);
-    }
+    };
+    console.log(input)
 
     const handleClick = () => {
         if (input.length === 0) {
             alert("Debe ingresar un valor")
         } else {
-            setTodos(todos.concat([input]));
+            setTodos([...todos, {"label":input, "done":false}]);
 			setInput("")
         }
     }
+    console.log(todos);
 
     const clickBorrar = () => {
         setTodos([])
     }
 
+    useEffect(() => {
+        // crearUser() // Solo necesito esta funcion una vez     
+    }, [])
+
+//   useEffect(() => {
+//     getTodos()
+//   uptdateTodos()
+//   },[todos])
+
+  function crearUser(){
+    fetch(`https://assets.breatheco.de/apis/fake/todos/user/tomasberni22`,
+    {method: 'POST', 
+    headers: {
+        'Content-Type': 'application/json'},
+    body: JSON.stringify([])
+  })
+    .then((response)=>response.json())
+    .then((data)=>console.log(data))
+}
+
+    function getTodos(){
+		fetch(``,
+		{method: 'GET',
+})
+		.then((response)=>response.json())
+		.then((data)=>setTodosEnServer(data))
+        .then((data)=>console.log(data))
+	}
+
+
+    function uptdateTodos(){
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/tomasberni22`,
+		{method: 'PUT', 
+		headers: {
+			'Content-Type': 'application/json'},
+		body: JSON.stringify(todos)
+	  })
+		.then((response)=>response.json())
+		.then((data)=>console.log(todosEnServer))
+	}
+
+    function killTodos(){
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/tomasberni22`,
+		{method: 'DELETE', 
+		headers: {
+			'Content-Type': 'application/json'}
+	  })
+		.then((response)=>response.json())
+		.then((data)=>console.log(data))
+	}
 
     return (
         <div className="container">
@@ -45,13 +98,13 @@ const Home = () => {
                     </div>
                     <div id="contenedorTodos" className="text-light">
                         {
-                        todos.map((item, index) => (
+                        todos.map((item, i) => (
                             <div className="row d-flex m-2"  style={{borderRadius:"30px",backgroundColor: "rgb(10, 19, 214)"}}>
                                 <div className="col-6">
                                     <h5 className="m-2">{item}</h5>
                                 </div>
                                 <div className="col-6 text-end">
-                                    <i class="fas fa-trash-alt align-items-end m-2 pt-1"
+                                    <i className="fas fa-trash-alt align-items-end m-2 pt-1"
                                         onClick={
                                             () => setTodos(todos.filter((elementoDiv, currentIndex) => index != currentIndex))
                                     }></i>
